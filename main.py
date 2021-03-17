8da824aefa856df4fea7b28143d6a57ad1a9a724
n4tm <nldm.eng20@uea.edu.br>
2021-03-17 15:23:33 -0400

    working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+from keep_alive import keep_alive
 import random
 
 #get client object
@@ -15,19 +16,23 @@ async def on_ready():
 #string variables
 en_greetings = ["Hi", "Hello", "Hoy"]
 pt_greetings = ["Oi", "Olá", "Eae"]
-emojis = ["! :grin:", "! :blush:", "! :grinning:", "! :slight_smile:", "! :upside_down:", "!"]
+emojis = ["!  :grin:", "!  :blush:", "!  :grinning:", "!  :slight_smile:", "!  :upside_down:", "!"]
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.lower().startswith(("hi chopper", "hello chopper", "hoy chopper")):
-        await message.channel.send(random.choice(en_greetings) + "! :grin:")
-    elif message.content.lower().startswith((
-            "oi chopper", "olá chopper", "salve chopper", "eae chopper", "fala chopper"
+    if message.content.lower().startswith((
+            "oi chopper", "olá chopper", "salve chopper", "eae chopper", "fala chopper", "chopper"
     )):
         await message.channel.send(random.choice(pt_greetings) + random.choice(emojis))
+    elif message.content.lower().startswith(("hi chopper", "hello chopper", "hoy chopper", "chopper")):
+        await message.channel.send(random.choice(en_greetings) + "! :grin:")
+
+
+#run continuously
+keep_alive()
 
 #get token
 load_dotenv('.env')
