2a338a64897871cd7da2466daa5a28ec1002da5a
n4tm <nldm.eng20@uea.edu.br>
2021-03-17 14:16:41 -0400

    Add bot greetings

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+import os
+from dotenv import load_dotenv
+import random
+
+#get client object
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged as {0.user}'.format(client))
+
+
+#string variables
+en_greetings = ["Hi", "Hello", "Hoy"]
+pt_greetings = ["Oi", "Olá", "Eae"]
+emojis = ["! :grin:", "! :blush:", "! :grinning:", "! :slight_smile:", "! :upside_down:", "!"]
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.lower().startswith(("hi chopper", "hello chopper", "hoy chopper")):
+        await message.channel.send(random.choice(en_greetings) + "! :grin:")
+    elif message.content.lower().startswith((
+            "oi chopper", "olá chopper", "salve chopper", "eae chopper", "fala chopper"
+    )):
+        await message.channel.send(random.choice(pt_greetings) + random.choice(emojis))
+
+#get token
+load_dotenv('.env')
+client.run(os.getenv('TOKEN'))
